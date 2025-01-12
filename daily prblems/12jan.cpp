503419f0f5433bf3a8a8b4340e9f6b021d1d73ba
jeevant010 <jeevantmudgil10@gmail.com>
2025-01-12 23:02:18 +0530

    new

---
+++ daily prblems/12jan.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+        // Trapping Rain Water
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int l = 0, r = arr.size() - 1, res = 0, lMax = 0, rMax = 0;
+        while (l < r) {
+            arr[l] < arr[r] ? (lMax = max(lMax, arr[l]), res += lMax - arr[l++]) : (rMax = max(rMax, arr[r]), res += rMax - arr[r--]);
+        }
+        return res;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
