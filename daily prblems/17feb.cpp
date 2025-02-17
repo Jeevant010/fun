df8128b8adf14cdc8045ec26349a3c12a551ec6f
jeevant010 <jeevantmudgil10@gmail.com>
2025-02-17 21:29:10 +0530

    commit

---
+++ daily prblems/17feb.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+        // Your code here
+        nth_element(arr.begin(), arr.end() - k, arr.end());
+        partial_sort(arr.end() - k, arr.end(), arr.end(), greater<int>());
+        return vector<int>(arr.end() - k, arr.end());
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.kLargest(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
