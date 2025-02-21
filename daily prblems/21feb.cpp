b522ea735cbe02de91c5a6eaf493fa1ad4bb5ff5
jeevant010 <jeevantmudgil10@gmail.com>
2025-02-21 21:07:18 +0530

    new

---
+++ daily prblems/21feb.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') st.push(c);
+            else if (st.empty() || abs(st.top() - c) > 2) return false;
+            else st.pop();
+        }
+        return st.empty();
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        Solution obj;
+        if (obj.isBalanced(a))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
