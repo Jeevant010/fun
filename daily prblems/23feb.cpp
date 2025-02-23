3402b55141f21ad3f257751b1501c56cb21491b1
jeevant010 <jeevantmudgil10@gmail.com>
2025-02-23 14:18:30 +0530

    new

---
+++ daily prblems/23feb.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the next greater element for each element of the array.
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        stack<int> s;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            int val = arr[i];
+            while (!s.empty() && s.top() <= val) s.pop();
+            arr[i] = s.empty() ? -1 : s.top();
+            s.push(val);
+        }
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading t
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Reading the entire input line for the array
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.nextLargerElement(a);
+
+        // Print the result in the required format
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
