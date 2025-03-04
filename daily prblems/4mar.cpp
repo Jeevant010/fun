2c80b77eee727f60c47d0feedc107d94ac68ee8f
jeevant010 <jeevantmudgil10@gmail.com>
2025-03-04 22:52:23 +0530

    levelup

---
+++ daily prblems/4mar.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
+        // code here
+        std::vector<int> ans;
+        for (int num : arr) {
+            auto it = std::lower_bound(ans.begin(), ans.end(), num);
+            if (it == ans.end()) ans.push_back(num);
+            else *it = num;
+        }
+        return ans.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
