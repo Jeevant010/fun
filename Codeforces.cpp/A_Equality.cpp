ec65d8dde360821a64cac9022c9289404998af85
jeevant010 <jeevantmudgil10@gmail.com>
2025-04-09 23:56:01 +0530

    new

---
+++ Codeforces.cpp/A_Equality.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long   
+
+void equality(int n,int m,string s){
+    
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    string s;
+    getline(cin,s);
+    equality(n,m,s);
+}
