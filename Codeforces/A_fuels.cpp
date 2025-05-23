ebb40a6bba7cf867be4d27f0d5b74c8514913e20
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-23 17:15:03 +0530

    changes

--- Codeforces/A_fuels.cpp
+++ Codeforces/A_fuels.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+
+int main(){
     int n, v;
     cin >> n >> v;
-    int ans = min(n - 1, v);
+    int a = min(n - 1, v);
     v = min(n - 1, v);
-    for (int i = 1; i < n; i++) {
-        if (n - i > v) {
-        ans += i + 1;
-        v++;
+    for(int i=1; i<n; i++){
+        if(n-i > v){
+            a += i+1;
+            v++;
         }
         v--;
     }
-    cout << ans << "\n";
+    cout << a << endl;
 }
