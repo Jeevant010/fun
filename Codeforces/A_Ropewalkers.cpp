7ef4c559fada8682e5ad05ab6284668dbbebccc1
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-23 23:55:41 +0530

    problem 1185A

--- Codeforces/A_Ropewalkers.cpp
+++ Codeforces/A_Ropewalkers.cpp
@@ -1,9 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main(){
-    int a, b, c, d;
+    ll a, b, c, d, e=0, f=0 ;
+    ll ans = 0;
     cin >> a >> b >> c >> d;
     
+    if(a > b){
+        swap(a,b);
+        if(a > c){
+            swap(a,c);
+        }
+    }
+    if(b > c){
+        swap(b,c);
+        if(a > b){
+            swap(a,b);
+        }
+    }
+
+    e = b - a;
+    f = c - b;
+    if(e < d){
+        ans += d - e;
+    }
+    if(f < d){
+        ans += d - f;
+    }
+    cout<<ans<<endl;
+
 
 }
