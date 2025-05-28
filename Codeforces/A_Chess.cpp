7e9ece317c3901b0d0ec5d87d5f9da293fa61634
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-28 21:31:15 +0530

    codeforces solution no 1075A

---
+++ Codeforces/A_Chess.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll n, a, b; 
+    cin >> n >> a >> b;
+    if(a+b <= n+1) cout<< "White" << endl;
+    else cout<< "Black" << endl;
+}
