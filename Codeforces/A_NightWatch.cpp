856aa69a7cb3379860c30f926decb12958b2af57
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-29 16:48:29 +0530

    codeforces 768 A solution

---
+++ Codeforces/A_NightWatch.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n, c = 0;
+    cin >> n;
+    vector<ll> a(n);
+    for(ll i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(ll i = 0; i < n; i++) {
+        if( a[i]>a[0] && a[i] < a[n-1] ) c++;
+    }
+    cout<< c << endl;
+    
+}
