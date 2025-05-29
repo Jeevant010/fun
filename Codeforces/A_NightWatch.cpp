477fc1a3f34030247b8fadcae3aeec2ba38338d3
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-29 16:50:35 +0530

    fixed code of 768 A code forces solution

--- Codeforces/A_NightWatch.cpp
+++ Codeforces/A_NightWatch.cpp
@@ -11,6 +11,7 @@ int main(){
     for(ll i = 0; i < n; i++) {
         cin >> a[i];
     }
+    sort(a.begin(), a.end());
     for(ll i = 0; i < n; i++) {
         if( a[i]>a[0] && a[i] < a[n-1] ) c++;
     }
