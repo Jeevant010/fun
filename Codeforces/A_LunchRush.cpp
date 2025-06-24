1936cb74eb71327f87bfac9017a8305cc78f7cbb
jeevant010 <jeevantmudgil10@gmail.com>
2025-06-24 23:59:17 +0530

    codeforces solution 169 276 A

---
+++ Codeforces/A_LunchRush.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define v vector
+#define p pair
+#define m unordered_map
+
+int main() {
+    ll n, k, mx = -1000000000; cin >> n >> k;
+    while(n--){
+        ll f, t; cin >> f >> t;
+        if(t <= k) mx = max(mx, f);
+        else mx = max(mx, (f - (t - k)));
+    }
+    cout << mx << endll;
+
+}
