717783df5f6442c7623eea0e113139f2a6af4c4b
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-08 18:20:00 +0530

    158 A codeforces solution

---
+++ Codeforces/A_Next_Round.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    int n,k, s = 0;
+    cin>>n>>k;
+    v<int> a(n);
+    r(i,n) cin>>a[i];
+    r(i,n){
+        if( a[i] >= a[k-1] && a[i]>0 ) s++;
+    }
+    cout<<s<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
