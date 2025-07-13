39a1343efd90453885d1215b72e5b1c208e54931
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-13 23:48:45 +0530

    977 A codeforces solution

---
+++ Codeforces/A_WrongSubtraction.cpp
@@ -0,0 +1,53 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+        long long n ,m;
+	cin>>n>>m;
+	for(int i=0;i<m;i++){
+		if(n%10==0){
+			n/=10;
+		}
+		else {
+			n-=1;
+		}
+	}
+	
+	
+	cout<<n;
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
