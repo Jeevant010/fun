978a7d3843665dd4c249d80d014c523a9d8e2637
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-14 21:12:56 +0530

    1977A codeforces solution

---
+++ Codeforces/A_Pass.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    string s;
+    cin>>s;
+    int n = s.length();
+    bool f = false;
+    if( n == 1){
+        if( s[0] != 'a') cout<<s<<'a'<<"\n";
+        else  cout<<s<<'b'<<"\n";
+        return;
+    }
+    else {
+        r(i,n){
+            if(s[i] == s[i+1] ){
+                s.insert(s.begin() + i+1, s[i] == 'a' ? 'b' : 'a');
+                f = true;
+                break;
+            }
+        }
+    }
+    if(f) cout<<s<<"\n";
+    else cout<<s<< (s[n-1] == 'a' ? 'b' : 'a') <<"\n";
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
