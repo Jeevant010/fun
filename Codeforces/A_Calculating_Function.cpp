edc6fcd9197ed8aa077b33cf568363e2d90d74d2
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-25 21:38:49 +0530

    486 A codeforces solution

---
+++ Codeforces/A_Calculating_Function.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    ll n;
+    cin>>n;
+    // 1 -1
+    // 2 1 <= -1 + 2
+    // 3 -2 <= -1 + 2 - 3
+    // 4 2 <= -1 + 2 - 3 + 4
+    // 6 3 <= -1 + 2 - 3 + 4 - 5 + 6
+    if( n&1 ) cout<<(-1)*(n/2 + 1)<<"\n";
+    else cout<<n/2<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
