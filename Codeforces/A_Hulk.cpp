7d30195b9da0d706e295a572716aa658ffdfa81c
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-26 10:21:58 +0530

    705 A codeforces solution

---
+++ Codeforces/A_Hulk.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    ll n;
+    cin>>n;
+    for(int i=1; i<n; i++){
+        cout<<"I ";
+        if( i&1  ) cout<<"hate ";
+        else cout<<"love ";
+        cout<<"that ";
+    }
+    if( n&1  ) cout<<"I hate ";
+        else cout<<"I love ";
+    cout<<"it";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
