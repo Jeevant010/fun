947dae54a6ae515dff9d78d8203fff00bfc84482
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-01 23:11:58 +0530

    2128 C codeforces solution

---
+++ Codeforces/A_Leftmost_below.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n , s = 1e9;
+    cin>>n;
+    bool f = true;
+    for(int i=0; i<n; i++){
+        int c; 
+        cin>>c;
+        if( c >= s*2 ) f = false;
+        s = min( s , c );
+    }
+    if( f ) cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
