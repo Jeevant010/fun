84c65d9147f348ab122e1a850b9abcaa201212d1
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-11 20:15:15 +0530

    solution codeforces 2131 B

---
+++ Codeforces/B_Alternating_Series.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+        int n;
+        cin >> n;
+		r(i ,n-1){
+            if(i % 2 == 0) cout << -1 << " ";
+            else cout << 3 << " ";
+        }
+    if(n % 2 == 0) cout << 2;
+    else cout << -1;
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+}
