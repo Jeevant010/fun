4d80ae528197dbba9ad25cf07999bc24cbfab085
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-13 21:47:43 +0530

    4 C registration systerm codeforces solution

---
+++ Codeforces/C_Registration_System.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n;
+    cin>>n;
+    ll x,m;
+    string s[100000];
+        map<string,int>mp;
+        r(i,n) cin>>s[i];
+        
+        r(i,n){
+            mp[s[i]]++;
+            x=mp[s[i]];
+            if(x==1)
+                cout<<"OK\n";
+            else
+                cout<<s[i]<<x-1<<endl;
+        }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t =1;
+    while (t--) {
+        solve();
+    }
+    
+}
