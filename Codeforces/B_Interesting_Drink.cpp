1052a8a263b8f8bb2a06d47d4693daeb5d9ba62a
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-04 00:28:33 +0530

    706 B codeforces solution

---
+++ Codeforces/B_Interesting_Drink.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define eb emplace_back
+#define pb push_back
+#define po pop_back
+#define pq priority_queue
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    ll t , n , q;
+    cin>>n;
+    v<int> sh;
+    
+    for(int i=0; i<n; i++) {
+            cin>>t;
+            sh.push_back(t);
+    }
+    sort(sh.begin(),sh.end());
+    cin>>q;
+    while(q--) {
+            cin>>t;
+            t=upper_bound(sh.begin(),sh.end(),t)-sh.begin();
+            cout<<t<<"\n";
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while( t-- ){
+        solve();
+    }
+    return 0;
+}
