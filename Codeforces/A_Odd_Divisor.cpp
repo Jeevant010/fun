a6d473837f5c0e1447f253d0b2d45cfb2fd35960
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-03 21:48:42 +0530

    1475 A codeforces solution

---
+++ Codeforces/A_Odd_Divisor.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define eb emplace_back
+#define pb push_back
+#define po pop_back
+#define pq priority_queue
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    ll n; 
+    cin>>n;
+    if( n % 2 ){
+        cout<<"YES"<<"\n";
+        return;
+    }
+    while (n % 2 == 0){
+        n /= 2;
+    }
+    
+    if(n>1) cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+        
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while( t-- ){
+        solve();
+    }
+    return 0;
+}
