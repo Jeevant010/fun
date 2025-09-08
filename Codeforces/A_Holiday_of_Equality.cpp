d2c2d7e63bae55db9314adeb9cee58b0d1249bba
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-08 13:00:17 +0530

    758 A codeforces solution

---
+++ Codeforces/A_Holiday_of_Equality.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define eb emplace_back
+#define pb push_back
+#define po pop_back
+#define pq priority_queue
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n; 
+    cin>>n;
+    v<int> a(n);
+    r(i ,n) cin>>a[i];
+    ll c = 0;
+    int ma = *max_element(a.begin(), a.end());
+    r(i,n){
+        c += (ma - a[i]);
+    }
+    cout<<c<<"\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while( t-- ){
+        solve();
+    }
+    return 0;
+}
