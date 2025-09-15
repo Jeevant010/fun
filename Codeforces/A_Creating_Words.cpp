d544229847f162b26305df4f1477d8b90554c7ef
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-15 19:44:07 +0530

    1985 A codeforces solution

---
+++ Codeforces/A_Creating_Words.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+#define pb push_back
+
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+        string s1, s2;
+        cin>>s1>>s2;
+        char temp = s1[0];
+        s1[0] = s2[0];
+        s2[0] = temp;
+        cout<<s1<<" "<<s2<<"\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
