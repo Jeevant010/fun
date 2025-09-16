d346db4e8573fb9cfe34f6eb8fd3094c2065cec5
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-16 22:43:57 +0530

    9 A codeforces solution

---
+++ Codeforces/A_Die_Roll.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    
+     int Y, W;
+    cin >> Y >> W;
+
+    string probability[7] = {"", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
+    int D = max(Y, W);
+    cout << probability[D] << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
