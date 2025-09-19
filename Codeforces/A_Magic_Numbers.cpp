feb2a40f80708ac95c0e69485c52ce21902a6d1e
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-19 22:53:31 +0530

    320 A codeforces solution

---
+++ Codeforces/A_Magic_Numbers.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+
+void solve() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < (int)s.size();) {
+        if (s.substr(i, 3) == "144") {
+            i += 3;
+        }
+        else if (s.substr(i, 2) == "14") {
+            i += 2;
+        }
+        else if (s[i] == '1') {
+            i += 1;
+        }
+        else {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
