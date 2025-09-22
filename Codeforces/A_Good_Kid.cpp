f7f55e46224c3e78da6fe9b23caaf8f0e144d8fc
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-22 15:10:05 +0530

    1873 B codeforces solution

---
+++ Codeforces/A_Good_Kid.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n;cin>>n;
+    v<int> ve(n);
+    r(i,n) cin>>ve[i];
+    auto ma = *min_element(trvs(ve));
+    bool f = false;
+    int c = 1;
+    r(i ,n){
+        if( ve[i] == ma && !f){
+            ve[i]++;
+            f = !f;
+        }
+        c *= ve[i];
+    }
+    cout<<c<<"\n";
+
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
