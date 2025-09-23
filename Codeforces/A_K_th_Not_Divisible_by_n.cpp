811380a9bd541602174ff0f29868aea556ca4011
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-23 23:00:10 +0530

    1352 C codeforces solution

---
+++ Codeforces/A_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n , k; 
+    cin>>n>>k;
+    cout<<((k - 1) / (n - 1)) + k<<"\n";
+
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
