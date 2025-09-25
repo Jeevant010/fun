3417fa2c9c8bb1d40a904f3d908c23ce412c2cde
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-25 20:16:40 +0530

    1367A codeforces solution

---
+++ Codeforces/A_Short_Substrings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    std::string b;
+        std::cin >> b;
+        std::string a = "";
+        a += b[0];
+        for (int i = 1; i < b.length(); i += 2) {
+            a += b[i]; 
+        }
+        std::cout << a <<"\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
