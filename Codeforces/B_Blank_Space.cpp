b2c80da2de11f9785665114f15b499bca8bf545e
jeevant010 <jeevantmudgil10@gmail.com>
2025-09-27 21:20:05 +0530

    1829 A codeforces solution

---
+++ Codeforces/B_Blank_Space.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0, curr = 0; i < n; i++) {
+        int a_i;
+        cin >> a_i;
+        if (a_i == 0) {
+            curr++;
+            ans = max(ans, curr);
+        } else {
+            curr = 0;
+        }
+    }
+    cout << ans << "\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t ;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
