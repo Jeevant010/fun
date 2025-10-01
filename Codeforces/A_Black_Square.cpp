16c9a2efec8f902962e2b2297c5e8630487b8dba
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-01 23:41:54 +0530

    431 A codeofrces solution

---
+++ Codeforces/A_Black_Square.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int calories[4]; 
+    for (int i = 0; i < 4; i++) {
+    cin >> calories[i];
+    }
+
+    string moves;
+    cin >> moves; 
+
+    int totalCalories = 0;
+    for (char move : moves) {
+    totalCalories += calories[move - '1'];
+    }
+
+    cout << totalCalories << endl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
