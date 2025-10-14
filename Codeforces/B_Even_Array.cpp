ff03f7c5de475cecfe03e05fbb1ae68816462794
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-14 21:35:11 +0530

    1367 B codeforces solution

---
+++ Codeforces/B_Even_Array.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int a[100] , n;
+
+    int c = 0,flag = 0;
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        int odd = 0,even = 0;
+        for(int i=0; i<n; i++){
+            if(i%2 != a[i] % 2){
+                if(a[i] % 2 == 1)odd++;
+                else even++;
+            }
+        }
+        if(odd != even){
+            cout<<-1<<"\n";
+        }
+        else{
+            cout<<even<<"\n";
+        }
+
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+}
