24e6e7d6738eab020dc98a16f05b2822c34f2d05
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-15 19:48:42 +0530

    707 A codeforces solution

---
+++ Codeforces/A_Brain_s_Photos.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    char ch;
+    int t, n, i, c=0;
+    cin>>t>>n;
+    for(i=1; i<=(t*n); i++) {
+        cin>>ch;
+        if(ch == 'B' || ch == 'W' || ch == 'G') {
+            c++;
+        }
+    }
+    if(c == (t*n)) {
+        cout<<"#Black&White\n";
+    }
+    else {
+        cout<<"#Color\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t  = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
