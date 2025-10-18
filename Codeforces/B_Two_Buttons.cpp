46000480ddfce725ca751c94aa82917cafcb1795
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-18 17:03:52 +0530

    520 B codeforces solution

---
+++ Codeforces/B_Two_Buttons.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    int n, m, w = 0;
+    cin >> n >> m;
+    while (n != m) {
+    if (m > n) {
+    if (m % 2 == 0)
+    m /= 2;
+    else
+    m++;
+    } else {
+    m++;
+    }
+    w++;
+    }
+    cout << w << "\n";
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
