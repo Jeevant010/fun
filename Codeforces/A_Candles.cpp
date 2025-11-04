c42ca1908dd59323f934aa0b3b6417df672e74ac
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-04 18:54:40 +0530

    379 A codeforces solution

---
+++ Codeforces/A_Candles.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    int totalHours = 0;
+    int leftovers = 0;
+
+    while (a > 0) {
+        totalHours += a;          
+        leftovers += a;           
+        a = leftovers / b;       
+        leftovers %= b;           
+    }
+
+    cout << totalHours << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+}
