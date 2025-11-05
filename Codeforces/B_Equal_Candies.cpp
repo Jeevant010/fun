0f61e12b85455ef6edd759fad330ab679927ac8a
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-05 10:27:27 +0530

    1676 B codeforces solution

---
+++ Codeforces/B_Equal_Candies.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    ll n, s = 0;
+    cin>>n;
+    v <int> vi(n);
+    r(i,n) cin>>vi[i];
+    int mi = *min_element(trvs(vi));
+    r(i,n) s = s + (vi[i] - mi);
+    cout<<s<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+}
