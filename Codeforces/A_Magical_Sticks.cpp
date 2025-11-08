d79989bdfe8a6d9c5952e1c7189b2105b0ff7ec0
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-08 18:18:51 +0530

    1371 A codeforces solution

---
+++ Codeforces/A_Magical_Sticks.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define eb emplace_back
+#define pb push_back
+#define po pop_back
+#define pq priority_queue
+#define INF 1e9
+
+
+void solve() {
+    int n;
+        cin >> n; // Number of sticks
+        // The minimum number of sticks required is (n + 1) / 2
+        cout << (n + 1) / 2 << "\n";
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while( t-- ){
+        solve();
+    }
+    return 0;
+}
