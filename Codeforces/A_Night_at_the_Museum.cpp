08bcc6c68fd30d53fb29671b0935ac6ce83cd31d
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-16 20:15:37 +0530

    731 A codeforces solution cpp

---
+++ Codeforces/A_Night_at_the_Museum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    string s;
+    cin>>s;
+    int res=0;
+    s='a'+s;
+    for(int i=0;i<s.length()-1;i++){
+        int val=abs(s[i]-s[i+1]);
+        if(val<=13)
+            res=res+val;
+        else
+            res=res+(26-val);
+    }
+    cout<<res;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
+ 
+ 
