8c7ffe7f47fdb8abdc3e9b1c0bc3c6208274fa78
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-19 22:15:18 +0530

    1881 A codeforces solution cpp

---
+++ Codeforces/A_Dont_try_to_Count.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    int n, m;
+    string x, s;
+    cin >> n >> m >> x >> s;
+
+    
+    for (int ops = 0; ops <= 6; ops++) {
+        if (x.find(s) != string::npos) { 
+            cout << ops << "\n";
+            return;
+        }
+        x += x; 
+    }
+    cout << -1 << "\n";
+        
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=0;
+    cin>>t;
+    while(t--)
+        solve();
+    
+    return 0;
+}
