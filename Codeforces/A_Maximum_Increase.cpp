273a1c0fe74be8910820c349b19ed78018309f0f
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-20 23:34:22 +0530

    702 A codeforces solution cpp

---
+++ Codeforces/A_Maximum_Increase.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    ll i,j,k;
+        ll n;
+
+        cin>>n;
+        ll a[n];
+
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+
+        ll count=1,max=1;
+
+        for(i=1;i<n;i++)
+        {
+            if(a[i]>a[i-1])
+                count++;
+            else
+            {
+                if(count>max)
+                    max=count;
+
+                count=1;
+            }
+        }
+
+        if(count>max)
+            max=count;
+
+        cout<<max<<"\n";
+        
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    while(t--)
+        solve();
+    
+    return 0;
+}
