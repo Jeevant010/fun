4868a357fc0d140729da5ea0b31baace232b524d
jeevant010 <jeevantmudgil10@gmail.com>
2025-11-30 18:24:02 +0530

    1992 A codeforces solution cpp

---
+++ Codeforces/A_Only_Pulses.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    ll a,b,c;cin>>a>>b>>c;
+
+    ll Max=0,k,na,nb,nc;
+    r(i,6)
+    {
+        r(j,6-i)
+        {
+            k = 5 - i - j;
+            Max = max(Max, (a+i) * (b+j) * (c+k));
+        }
+    }
+    cout<<Max<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+}
