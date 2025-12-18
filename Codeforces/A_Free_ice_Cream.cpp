9b078c36c60b7689840597f220d318a7e8de5a55
jeevant010 <jeevantmudgil10@gmail.com>
2025-12-18 20:51:25 +0530

    686 A codeforces solution cpp

---
+++ Codeforces/A_Free_ice_Cream.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    long long n,x,d,count=0;
+    char c;
+    cin>>n>>x;
+
+    for(int i=0;i<n;i++){
+        cin>>c>>d;
+        if(c=='+') x+=d;
+        
+        else if(c=='-'){
+            if((x-d)>=0) x-=d;
+            else count++;
+        }
+    }
+    cout<<x<<" "<<count<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+}
