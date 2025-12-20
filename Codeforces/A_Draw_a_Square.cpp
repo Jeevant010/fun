40451edf9108b661ad7bdefbbc2f6c52415767c2
jeevant010 <jeevantmudgil10@gmail.com>
2025-12-20 17:55:57 +0530

    2074 A codeforces solution cpp

---
+++ Codeforces/A_Draw_a_Square.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    int l, r, u, d;
+    cin>>l>>r>>u>>d;
+    if( l == r && d == u){
+        cout<<"YES"<<"\n";
+        return;
+    }
+    else {
+        cout<<"NO"<<"\n";
+        return;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+}
