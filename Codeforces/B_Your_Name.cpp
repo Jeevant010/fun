49f4badca84bc792ff680eb843cd50104b2c25be
jeevant010 <jeevantmudgil10@gmail.com>
2025-12-24 22:11:39 +0530

    2167 B codeforces solution cpp

---
+++ Codeforces/B_Your_Name.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    ll n;cin>>n;
+    string s,t;cin>>s>>t;
+    sort(trvs(s));
+    sort(trvs(t));
+    if(s==t)
+    cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t =1;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
