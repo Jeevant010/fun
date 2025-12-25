09050980de2f8d073f64cbbe1d193bfa83bb6ecc
jeevant010 <jeevantmudgil10@gmail.com>
2025-12-25 22:36:30 +0530

    1337 A codeforces solution cpp

---
+++ Codeforces/A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+    long long int a,b,c,d;
+    cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
