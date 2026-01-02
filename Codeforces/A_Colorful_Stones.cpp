de6c8d52b1d228000f16fc63a21f2ff56a64a2d6
jeevant010 <jeevantmudgil10@gmail.com>
2026-01-02 22:32:34 +0530

    265 A codeforces solution cpp

---
+++ Codeforces/A_Colorful_Stones.cpp
@@ -0,0 +1,69 @@
+
+
+    #include<bits/stdc++.h>
+#define endl '\n'
+#define time clock_t tStart = clock();
+#define show printf("Time taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#define ll                    long long int
+#define r(a ,n)             for(int i=a;i<n;++i)
+#define count_1(n)            __builtin_popcountll(n)
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define itoc(c)               ((char)(((int)'0')+c))
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define eb                    emplace_back
+using namespace std;
+
+
+
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    int pos = 0;
+    for (int i = 0; i < t.length(); ++i)
+    {
+        if (t[i] == s[pos])
+        {
+            pos += 1;
+        }
+    }
+    cout << pos + 1 << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
