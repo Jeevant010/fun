cde5005dd9122b2e210a80599616e963e014fbad
jeevant010 <jeevantmudgil10@gmail.com>
2026-02-04 21:25:24 +0530

    777 A codeforces solution cpp

---
+++ Codeforces/A_Shell_Game.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define r(i,n) for(ll i=0; i<n; i++)
+#define trvs(a) a.begin(), a.end()
+#define v vector
+#define p pair
+#define m unordered_map
+#define INF 1e9
+
+ll gcd(ll a, ll b){
+    if( b>a ) swap(a,b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b ){
+    return abs(a * b) / gcd(a, b);
+}
+
+ll og2(ll x){
+    return (64 - __builtin_clzll(x) - 1);
+}
+
+
+void solve() {
+
+    int n,x;
+    cin>>n>>x;
+    n%=6;
+    int a[4];
+    a[0]=0,a[1]=1,a[2]=2;
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+            swap(a[0],a[1]);
+        else
+            swap(a[1],a[2]);
+    }
+    cout<<a[x]<<"\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
